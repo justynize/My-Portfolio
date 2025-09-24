2b7ed7db420a600ce94652bc6ffdb203cd222164
justynize <nwobajusticeekene12@gamil.com>
2025-09-24 22:33:48 +0100

    updated

---
+++ file.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.text', {
+    strings: ["Web Developer", "UI/UX Designer", "Graphic Designer", "Video Editor"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
